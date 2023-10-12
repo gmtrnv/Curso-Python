f5a0efc67d8960a495d94e2d765bfceee4814378
Guillermo <gmtrnv@gmail.com>
2023-10-12 17:13:54 -0600

    agregando sublistas

--- 02 Listas/listas.py
+++ 02 Listas/listas.py
@@ -3,12 +3,17 @@
 lista = ['String', 10, 77.2, True]
 #Crear listas que sólo almacenen un tipo de valor
 
-lista_cursos = ['Python', 'Django',  'Flask', 'Java']
+lista_cursos = ['Python', 'Django',  'Flask', 'Java', 'Ruby']
+#                  0         1          2       3        4
 lista_cursos[1] = 'Rust'
+
+#Las sublistas son inclusivas a la izquierda y exclusivas a la derecha
+#[start:end]
+sub_lista = lista_cursos[0:3]
 
 primer_curso = lista_cursos[0]
 ultimo_curso = lista_cursos[-1]
 
-print(lista_cursos)
+print(sub_lista)
 print("Primer curso: " + primer_curso)
 print("Último curso: " + ultimo_curso)
