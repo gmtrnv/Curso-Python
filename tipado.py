9de0c474914566bef46cd1ff471690a3f81d2b6d
Guillermo <gmtrnv@gmail.com>
2023-10-12 15:04:51 -0600

    agregando tipos al tipado

--- tipado.py
+++ tipado.py
@@ -1,7 +1,20 @@
 valor = "Fulanito"
+#podemos conocer el tipo de dato con la función type
+tipo = type(valor)
+print(tipo)
+
 valor = 2
+tipo = type(valor)
+print(tipo)
+
 valor = 3.1
+tipo = type(valor)
+print(tipo)
+
 valor = True
+tipo = type(valor)
+print(tipo)
+
 #Python tiene tipado dinámico
 #puede cambiar de tipo de variable en tiempo de ejecución
 print(valor)
