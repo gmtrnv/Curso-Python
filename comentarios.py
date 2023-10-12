0768372c9a9a9510a6a863ca67ba5886bc848aca
Guillermo <gmtrnv@gmail.com>
2023-10-12 14:15:02 -0600

    agregando comentarios y tipos

---
+++ comentarios.py
@@ -0,0 +1,8 @@
+# Una sola linea comentada
+print('Hola mundo')
+
+'''
+Este es un comentario
+de multiples
+lineas
+'''
---
+++ tipos.py
@@ -0,0 +1,31 @@
+#String
+titulo_curso = 'Curso profesional de Python'
+print(titulo_curso)
+
+nombre_completo = "Fulanito Detal"
+print(nombre_completo)
+
+dobles = '"Comillas Dobles"'
+simples = "'comillas simples'"
+multiples = '''Saltos
+de linea
+multiples
+'''
+
+
+print(dobles)
+print(simples)
+print('-----------------')
+print(multiples)
+
+#Int
+entero = 10 + 5
+print(entero)
+
+#float
+flotante = 3.14 * 2.45
+print(flotante)
+
+#Bool
+booleano = False
+print(booleano)
