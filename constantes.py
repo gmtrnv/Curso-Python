b64070655a0cb39755ea51fafe2d8bd27c7ded7c
Guillermo <gmtrnv@gmail.com>
2023-10-12 12:55:06 -0600

    Agregando explicación a constantes

--- constantes.py
+++ constantes.py
@@ -1,1 +1,6 @@
-TITULO_CURSO = "Curso profesionla de python"
+TITULO_CURSO = 'Curso profesionla de python'
+
+#Se puede cambiar la constante, pero por convención si está en mayúsculas no debería modificarse manualmente
+# TITULO_CURSO = 'python'
+
+print(TITULO_CURSO)
