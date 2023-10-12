fc621fc305d6749d528a7ca03706503dc0683355
Guillermo <gmtrnv@gmail.com>
2023-10-12 15:05:34 -0600

    optimizando codigo

--- tipado.py
+++ tipado.py
@@ -4,16 +4,13 @@ tipo = type(valor)
 print(tipo)
 
 valor = 2
-tipo = type(valor)
-print(tipo)
+print(type(valor))
 
 valor = 3.1
-tipo = type(valor)
-print(tipo)
+print(type(valor))
 
 valor = True
-tipo = type(valor)
-print(tipo)
+print(type(valor))
 
 #Python tiene tipado dinámico
 #puede cambiar de tipo de variable en tiempo de ejecución
