7cdc4843d932423f565141980153862829927f9e
Guillermo <gmtrnv@gmail.com>
2023-10-14 13:44:31 -0600

    organizando listas

--- 02 Listas/listas.py
+++ 02 Listas/listas.py
@@ -9,7 +9,13 @@ lista_cursos[1] = 'Rust'
 
 #Las sublistas son inclusivas a la izquierda y exclusivas a la derecha
 #[start:end]
-sub_lista = lista_cursos[0:3]
+#sub_lista = lista_cursos[0:3] -> Obtenemos un rango
+#sub_lista = lista_cursos[1:] -> Obtenemos los últimos
+#sub_lista = lista_cursos[:3] -> Obtenemos los primeros
+#sub_lista = lista_cursos[0:4:2] -> Genera saltos 
+#[start:end:skip] todos los valores son opcionales
+#si usamos -1 como el valor de skip invertimos el orden de la lista
+sub_lista = lista_cursos[::-1]
 
 primer_curso = lista_cursos[0]
 ultimo_curso = lista_cursos[-1]
