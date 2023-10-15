2df42d8e6ae4f8fedb3ba2d968f43b1c9e2805f6
Guillermo <gmtrnv@gmail.com>
2023-10-15 00:45:51 -0600

    alineacion de strings

---
+++ 04 Strings/alineacion.py
@@ -0,0 +1,12 @@
+mensaje = 'Hola mundo!'
+
+print(mensaje)
+
+#ljust -> justifica a la izquirda
+#rjust -> justifica a la derecha
+#center -> justifica al centro
+
+#agregar la cantidad de espacios a agregar como valor que le pasamos a la función
+print(mensaje.rjust(20), '.')
+
+print(mensaje.center(20))
