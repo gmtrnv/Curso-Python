9cc6d7272d3214406976488c769b90006058eb3e
Guillermo <gmtrnv@gmail.com>
2023-10-15 00:18:40 -0600

    agregando funciones directo al print

--- 04 Strings/Fstrings.py
+++ 04 Strings/Fstrings.py
@@ -6,4 +6,7 @@ edad = 98
 #utiliizamos los nombres directamente de las variables, sólo hay que anteponer una "f" antes del string
 #también podemos usar operaciones dentro
 nombre_completo = f'Mr. {nombre} {apellido}, tiene {edad}, recuerda {edad} años y mide {1.42}m!! aún le quedan {edad * 2} de años'
-print(nombre_completo)
+
+#print(nombre_completo)
+#se puede imprimir directamente
+print(f'Mr. {nombre} {apellido}, tiene {edad}, recuerda {edad} años y mide {1.42}m!! aún le quedan {edad * 2} de años')
