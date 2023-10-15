a039b3d373b06ca6cd89dbeaccb733730f206a3f
Guillermo <gmtrnv@gmail.com>
2023-10-15 00:17:36 -0600

    usando interpolación con fstring

---
+++ 04 Strings/Fstrings.py
@@ -0,0 +1,9 @@
+nombre = 'Alguienuel'
+apellido = "Apellidez"
+edad = 98
+
+#podemos usar fstrings para hacer interpolación
+#utiliizamos los nombres directamente de las variables, sólo hay que anteponer una "f" antes del string
+#también podemos usar operaciones dentro
+nombre_completo = f'Mr. {nombre} {apellido}, tiene {edad}, recuerda {edad} años y mide {1.42}m!! aún le quedan {edad * 2} de años'
+print(nombre_completo)
