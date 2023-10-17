0964b6bb3d332a0bfc2886dd88dc258f1814ed8a
Guillermo <gmtrnv@gmail.com>
2023-10-17 01:51:31 -0600

    visualizando llaves, valores y elementos

---
+++ 05 Diccionarios/llaves.py
@@ -0,0 +1,8 @@
+diccionario = {'a':1,'b':2,'c':3,'d':4}
+llaves = tuple(diccionario.keys())
+valores = tuple(diccionario.values())
+elementos = tuple(diccionario.items())
+print(diccionario)
+print(llaves)
+print(valores)
+print(elementos)
