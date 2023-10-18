aeb348454aaf4f8d844c72525ed69a0ed7d62bac
Guillermo <gmtrnv@gmail.com>
2023-10-17 21:06:12 -0600

    condiciones

---
+++ 06 Condiciones/condiciones.py
@@ -0,0 +1,17 @@
+resultado = 15
+
+"""
+if <Bool>:
+(4espacios)condición
+"""
+# if resultado:
+#     print('La variable cumple al condición')
+
+if resultado > 10 and resultado < 20:
+    print('hola mundo')
+else: 
+    print('adios mundo')
+
+resultado = resultado > 10
+
+
