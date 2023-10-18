0af6a5d60560c3fa7e14eb01b9c02eb23b2de3e2
Guillermo <gmtrnv@gmail.com>
2023-10-17 21:26:37 -0600

    foreach

---
+++ 06 Condiciones/foreach.py
@@ -0,0 +1,7 @@
+#for usa cualquier objeto iterable: listas, tuplas, diccionaro, etc
+usuarios = ['Fulanito', 'Alguienuel', 'Detalien', 'Nombreriel']
+
+#for <variable interna> in <objeto iterable>:
+#   codigo que hace uso de la variable interna
+for usuario in usuarios:
+    print(usuario)
