2440a927f116c21d28471bfa54cc12abe0acd301
Guillermo <gmtrnv@gmail.com>
2023-10-17 21:01:08 -0600

    none es falso

--- 06 Condiciones/none.py
+++ 06 Condiciones/none.py
@@ -1,3 +1,23 @@
+#podemos crear una variable de tipo none
+#None es un resultado "Falso"
 resultado = None
 
+#y luego cambiar su tipo de manera dinamica
+resultado = [1,2,3]
+
 print(resultado)
+print(type(resultado))
+
+#True = 1 / False = 0
+#Los siguientes son tomados por python como "FALSE"
+"""
+False
+None
+0
+0.0
+'' / ""
+[]
+()
+{}
+
+"""
