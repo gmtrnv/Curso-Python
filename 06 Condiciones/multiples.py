eba0019a3464a6c3c897e158d092a758b189820b
Guillermo <gmtrnv@gmail.com>
2023-10-17 21:10:18 -0600

    elif

---
+++ 06 Condiciones/multiples.py
@@ -0,0 +1,9 @@
+calificacion = 9.999999999
+
+if calificacion == 10:
+    print('Excelencia')
+elif calificacion < 10 and calificacion >= 6:
+    print('La calificación es aprobatoria')
+else:
+    print('Reprobaste la materia')
+
