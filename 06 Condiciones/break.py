72b04ec9e3c75c5e3b7bc782c5b7c5edd41ac00c
Guillermo <gmtrnv@gmail.com>
2023-10-17 21:30:02 -0600

    break-continue

---
+++ 06 Condiciones/break.py
@@ -0,0 +1,9 @@
+titulo_curso = 'Curso profesional de Python'
+
+for caracter in titulo_curso:
+    if caracter == ' ':
+    #break finaliza el ciclo
+        #break
+    #continue salta un espacio de codigo
+        continue
+    print(caracter)
